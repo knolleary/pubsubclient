f8cd24f0ef6c858292fa4a40026d62d6de64bfb3
Nicholas O'Leary <nick.oleary@gmail.com>
2012-11-08 21:45:41 +0000

    rewrite testsuite in python

---
+++ tests/testcases/mqtt_basic.py
@@ -0,0 +1,12 @@
+import unittest
+
+class mqtt_basic(unittest.TestCase):
+  def setUp(self):
+    pass
+    
+    
+  def test_one(self):
+    self.assertEqual(3,3)
+
+  def test_two(self):
+    self.assertEqual(4,4)
