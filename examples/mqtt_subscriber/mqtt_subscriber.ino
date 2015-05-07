d9edf64ad4aca5356d0541a7bd7f34ce40adf166
Ian Tester <imroykun@gmail.com>
2015-05-07 23:15:43 +1000

    Add simple subscriber example
    Prints the topic and payload of every message it receives.

---
+++ examples/mqtt_subscriber/mqtt_subscriber.ino
@@ -0,0 +1,56 @@
+/*
+ MQTT subscriber example
+
+  - connects to an MQTT server
+  - subscribes to the topic "inTopic"
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char *ssid =	"xxxxxxxx";		// cannot be longer than 32 characters!
+const char *pass =	"yyyyyyyy";		//
+
+// Update these with values suitable for your network.
+IPAddress server(172, 16, 0, 2);
+
+void callback(MQTT::Publish& pub) {
+  Serial.print(pub.topic());
+  Serial.print(" => ");
+  Serial.print(pub.payload_string());
+}
+
+PubSubClient client(server);
+
+void setup()
+{
+  // Setup console
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+  Serial.println();
+
+  client.set_callback(callback);
+
+  WiFi.begin(ssid, pass);
+
+  int retries = 0;
+  while ((WiFi.status() != WL_CONNECTED) && (retries < 10)) {
+    retries++;
+    delay(500);
+    Serial.print(".");
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.println("WiFi connected");
+  }
+
+  if (client.connect("arduinoClient")) {
+    client.subscribe("inTopic");
+  }
+}
+
+void loop()
+{
+  client.loop();
+}
