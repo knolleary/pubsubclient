c549318e86496d827610af4666db5c3014de7ba0
Ian Tester <imroykun@gmail.com>
2015-05-17 03:00:31 +1000

    Add comments to other PubSubClient methods

--- src/PubSubClient.h
+++ src/PubSubClient.h
@@ -49,24 +49,47 @@ public:
    PubSubClient& set_server(IPAddress &ip, uint16_t port = 1883);
    PubSubClient& set_server(String hostname, uint16_t port = 1883);
 
+   // Get or set the callback function
    callback_t callback(void) const { return _callback; }
    PubSubClient& set_callback(callback_t cb) { _callback = cb; return *this; }
    PubSubClient& unset_callback(void) { _callback = NULL; return * this; }
 
+   // Set the maximum number of retries when waiting for response packets
    PubSubClient& set_max_retries(uint8_t mr) { _max_retries = mr; return *this; }
 
+   // Connect to the server with a client id
    bool connect(String id);
+
+   // Connect to the server with a client id and "will" parameters
    bool connect(String id, String willTopic, uint8_t willQos, bool willRetain, String willMessage);
+
+   // Disconnect from the server
    void disconnect(void);
+
+   // Publish a string payload
    bool publish(String topic, String payload);
+
+   // Publish an arbitrary data payload
    bool publish(String topic, const uint8_t *payload, unsigned int plength, bool retained = false);
+
+   // Publish an arbitrary data payload stored in "program memory"
    bool publish_P(String topic, PGM_P payload, unsigned int, bool retained = false);
+
+   // Subscribe to a topic
    bool subscribe(String topic, uint8_t qos = 0);
+
+   // Unsubscribe from a topic
    bool unsubscribe(String topic);
 
+   // Wait for packets to come in, processing them
+   // Also periodically pings the server
    bool loop();
+
+   // Are we connected?
    bool connected();
 
+   // Return the next packet id
+   // Needed for constructing our own publish (with QoS>0) or (un)subscribe messages
    uint16_t next_packet_id(void) {
      nextMsgId++;
      if (nextMsgId == 0) nextMsgId = 1;
