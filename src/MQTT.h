b9e68d6b3b5d2219e0980268b584a4d2a68e650c
Ian Tester <imroykun@gmail.com>
2015-06-22 23:41:02 +1000

    Convert the comments in MQTT.h to Doxygen documentation

--- src/MQTT.h
+++ src/MQTT.h
@@ -44,58 +44,95 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 class PubSubClient;
 
+//! namespace for classes representing MQTT messages
 namespace MQTT {
-  // Abstract base class
+
+  //! Abstract base class
   class Message {
   protected:
     uint8_t _type, _flags;
-    uint16_t _packet_id;	// Not all message types use a packet id, but most do
+    uint16_t _packet_id;	//! Not all message types use a packet id, but most do
 
+    //! Private constructor from type and flags
     Message(uint8_t t, uint8_t f = 0) :
       _type(t), _flags(f),
       _packet_id(0)
     {}
 
+    //! Private constructor from type and packet id
     Message(uint8_t t, uint16_t pid) :
       _type(t), _flags(0),
       _packet_id(pid)
     {}
 
+    //! Virtual destructor
     virtual ~Message() {}
 
-    // Write the fixed header to a buffer
+    //! Length of the fixed header
+    /*!
+      \param rlength Remaining lengh i.e variable header + payload
+    */
     uint8_t fixed_header_length(uint32_t rlength) const;
+
+    //! Write the fixed header to a buffer
+    /*!
+      \param buf Pointer to start of buffer (never advances)
+      \param bufpos Current position in buffer
+      \param rlength Remaining lengh i.e variable header + payload
+    */
     void write_fixed_header(uint8_t *buf, uint32_t& bufpos, uint32_t rlength) const;
 
+    //! Write the packet id to a buffer
+    /*!
+      \param buf Pointer to start of buffer (never advances)
+      \param bufpos Current position in buffer
+    */
     void write_packet_id(uint8_t *buf, uint32_t& bufpos) const;
 
-    // Abstract methods to be implemented by derived classes
+    //! Length of variable header
     virtual uint32_t variable_header_length(void) const { return 0; }
+
+    //! Write variable header
+    /*!
+      \param buf Pointer to start of buffer (never advances)
+      \param bufpos Current position in buffer
+    */
     virtual void write_variable_header(uint8_t *buf, uint32_t& bufpos) const { }
+
+    //! Length of payload
     virtual uint32_t payload_length(void) const { return 0; }
+
+    //! Write payload
+    /*!
+      \param buf Pointer to start of buffer (never advances)
+      \param bufpos Current position in buffer
+    */
     virtual void write_payload(uint8_t *buf, uint32_t& bufpos) const { }
 
+    //! Message type to expect in response to this message
     virtual uint8_t response_type(void) const { return 0; }
 
     friend PubSubClient;	// Just to allow it to call response_type()
 
   public:
-    // Send the message out
+    //! Send the message out
     bool send(WiFiClient& wclient);
 
-    // Get the message type
+    //! Get the message type
     uint8_t type(void) const { return _type; }
 
-    // Get the packet id
+    //! Get the packet id
     uint16_t packet_id(void) const { return _packet_id; }
   };
 
-  // Parser
-  // remember to free the object once you're finished with it
+  //! Parser
+  /*!
+    remember to free the object once you're finished with it
+  */
   Message* readPacket(WiFiClient &client);
 
 
-  // Message sent when connecting to a broker
+  //! Message sent when connecting to a broker
   class Connect : public Message {
   private:
     bool _clean_session;
@@ -117,44 +154,48 @@ namespace MQTT {
     uint8_t response_type(void) const { return MQTTCONNACK; }
 
   public:
-    // Connect with a client ID
+    //! Connect with a client ID
     Connect(String cid);
 
-    // Set or unset the "clear session" flag
+    //! Set the "clear session" flag
     Connect& set_clean_session(bool cs = true)	{ _clean_session = cs; return *this; }
+    //! Unset the "clear session" flag
     Connect& unset_clean_session(void)		{ _clean_session = false; return *this; }
 
-    // Set or unset the "will" flag and associated attributes
+    //! Set the "will" flag and associated attributes
     Connect& set_will(String willTopic, String willMessage, uint8_t willQos = 0, bool willRetain = false) {
       _will_topic = willTopic; _will_message = willMessage; _will_qos = willQos; _will_retain = willRetain;
       return *this;
     }
+    //! Unset the "will" flag and associated attributes
     Connect& unset_will(void)			{ _will_topic = ""; return *this; }
 
-    // Set or unset the username and password for authentication
+    //! Set the username and password for authentication
     Connect& set_auth(String u, String p)	{ _username = u; _password = p; return *this; }
+    //! Unset the username and password for authentication
     Connect& unset_auth(void)			{ _username = ""; _password = ""; return *this; }
 
-    // Get or set the keepalive period
+    //! Get the keepalive period
     uint16_t keepalive(void) const	{ return _keepalive; }
+    //! Set the keepalive period
     Connect& set_keepalive(uint16_t k)	{ _keepalive = k; return *this; }
 
   };
 
 
-  // Response to Connect
+  //! Response to Connect
   class ConnectAck : public Message {
   private:
     bool _session_present;
     uint8_t _rc;
 
   public:
-    // Construct from a network buffer
+    //! Construct from a network buffer
     ConnectAck(uint8_t* data, uint32_t length);
   };
 
 
-  // Publish a payload to a topic
+  //! Publish a payload to a topic
   class Publish : public Message {
   private:
     String _topic;
@@ -169,6 +210,7 @@ namespace MQTT {
 
     uint8_t response_type(void) const;
 
+    //! Private constructor from a payload and allowing _payload_mine to be set
     Publish(String topic, uint8_t* payload, uint32_t length, bool mine) :
       Message(MQTTPUBLISH),
       _topic(topic),
@@ -177,64 +219,74 @@ namespace MQTT {
     {}
 
   public:
-    // Constructors for creating our own message to publish
+    //! Constructors from string payload
     Publish(String topic, String payload);
+
+    //! Constructors from arbitrary payload
     Publish(String topic, uint8_t* payload, uint32_t length) :
       Publish(topic, payload, length, false)
     {}
 
-    // Constructor using a string stored in flash using the F() macro
+    //! Constructor from a string stored in flash using the F() macro
     Publish(String topic, const __FlashStringHelper* payload);
-    // A function made to look like a constructor, reading the payload from flash
+
     friend Publish Publish_P(String topic, PGM_P payload, uint32_t length);
 
-    // Construct from a network buffer
+    //! Construct from a network buffer
     Publish(uint8_t flags, uint8_t* data, uint32_t length);
 
     ~Publish();
 
-    // Get or set retain flag
+    //! Get retain flag
     bool retain(void) const		{ return _flags & 0x01; }
+    //! Set retain flag
     Publish& set_retain(bool r = true)	{ _flags = (_flags & ~0x01) | r; return *this; }
+    //! Unset retain flag
     Publish& unset_retain(void)		{ _flags = _flags & ~0x01; return *this; }
 
-    // Get or set QoS value
+    //! Get QoS value
     uint8_t qos(void) const		{ return (_flags >> 1) & 0x03; }
+    //! Set QoS value
     Publish& set_qos(uint8_t q, uint16_t pid = 0);
+    //! Unset QoS value
     Publish& unset_qos(void)		{ _flags &= ~0x06; return *this; }
 
-    // Get or set dup flag
+    //! Get dup flag
     bool dup(void) const		{ return (_flags >> 3) & 0x01; }
+    //! Set dup flag
     Publish& set_dup(bool d = true)	{ _flags = (_flags & ~0x08) | (d ? 0x08 : 0); return *this; }
+    //! Unset dup flag
     Publish& unset_dup(void)		{ _flags = _flags & ~0x08; return *this; }
 
-    // Stopic string
+    //! Get the topic string
     String topic(void) const { return _topic; }
 
-    // Payload as a string
+    //! Get the payload as a string
     String payload_string(void) const;
 
-    // Get the payload pointer and length
+    //! Get the payload pointer
     uint8_t* payload(void) const { return _payload; }
+    //! Get the payload length
     uint32_t payload_len(void) const { return _payload_len; }
 
   };
 
+  //! A function made to look like a constructor, reading the payload from flash
   Publish Publish_P(String topic, PGM_P payload, uint32_t length);
 
 
-  // Response to Publish when qos == 1
+  //! Response to Publish when qos == 1
   class PublishAck : public Message {
   public:
-    // Construct with a packet id
+    //! Construct with a packet id
     PublishAck(uint16_t pid);
 
-    // Construct from a network buffer
+    //! Construct from a network buffer
     PublishAck(uint8_t* data, uint32_t length);
   };
 
 
-  // First response to Publish when qos == 2
+  //! First response to Publish when qos == 2
   class PublishRec : public Message {
   private:
     uint32_t variable_header_length(void) const;
@@ -243,16 +295,16 @@ namespace MQTT {
     uint8_t response_type(void) const { return MQTTPUBREL; }
 
   public:
-    // Construct with a packet id
+    //! Construct with a packet id
     PublishRec(uint16_t pid);
 
-    // Construct from a network buffer
+    //! Construct from a network buffer
     PublishRec(uint8_t* data, uint32_t length);
 
   };
 
 
-  // Response to PublishRec
+  //! Response to PublishRec
   class PublishRel : public Message {
   private:
     uint32_t variable_header_length(void) const;
@@ -261,31 +313,31 @@ namespace MQTT {
     uint8_t response_type(void) const { return MQTTPUBCOMP; }
 
   public:
-    // Construct with a packet id
+    //! Construct with a packet id
     PublishRel(uint16_t pid);
 
-    // Construct from a network buffer
+    //! Construct from a network buffer
     PublishRel(uint8_t* data, uint32_t length);
 
   };
 
 
-  // Response to PublishRel
+  //! Response to PublishRel
   class PublishComp : public Message {
   private:
     uint32_t variable_header_length(void) const;
     void write_variable_header(uint8_t *buf, uint32_t& bufpos) const;
 
   public:
-    // Construct with a packet id
+    //! Construct with a packet id
     PublishComp(uint16_t pid);
 
-    // Construct from a network buffer
+    //! Construct from a network buffer
     PublishComp(uint8_t* data, uint32_t length);
   };
 
 
-  // Subscribe to one or more topics
+  //! Subscribe to one or more topics
   class Subscribe : public Message {
   private:
     uint8_t *_buffer;
@@ -299,42 +351,42 @@ namespace MQTT {
     uint8_t response_type(void) const { return MQTTSUBACK; }
 
   public:
-    // Constructor that starts with a packet id and empty list of subscriptions
+    //! Constructor that starts with a packet id and empty list of subscriptions
     Subscribe(uint16_t pid);
 
-    // Subscribe with a packet id, topic, and optional QoS level
+    //! Subscribe with a packet id, topic, and optional QoS level
     Subscribe(uint16_t pid, String topic, uint8_t qos = 0);
 
     ~Subscribe();
 
-    // Add another topic and optional QoS level
+    //! Add another topic and optional QoS level
     Subscribe& add_topic(String topic, uint8_t qos = 0);
 
   };
 
 
-  // Response to Subscribe
+  //! Response to Subscribe
   class SubscribeAck : public Message {
   private:
     uint8_t *_rcs;
     uint32_t _num_rcs;
 
   public:
-    // Construct from a network buffer
+    //! Construct from a network buffer
     SubscribeAck(uint8_t* data, uint32_t length);
 
     ~SubscribeAck();
 
-    // Get the number of return codes available
+    //! Get the number of return codes available
     uint32_t num_rcs(void) const { return _num_rcs; }
 
-    // Get a return code
+    //! Get a return code
     uint8_t rc(uint8_t i) const { return _rcs[i]; }
 
   };
 
 
-  // Unsubscribe from one or more topics
+  //! Unsubscribe from one or more topics
   class Unsubscribe : public Message {
   private:
     uint8_t *_buffer;
@@ -348,41 +400,41 @@ namespace MQTT {
     uint8_t response_type(void) const { return MQTTUNSUBACK; }
 
   public:
-    // Constructor that starts with a packet id and empty list of unsubscriptions
+    //! Constructor that starts with a packet id and empty list of unsubscriptions
     Unsubscribe(uint16_t pid);
 
-    // Unsubscribe from a topic, with a packet id
+    //! Unsubscribe from a topic, with a packet id
     Unsubscribe(uint16_t pid, String topic);
 
     ~Unsubscribe();
 
-    // Add another topic to unsubscribe from
+    //! Add another topic to unsubscribe from
     Unsubscribe& add_topic(String topic);
 
   };
 
 
-  // Response to Unsubscribe
+  //! Response to Unsubscribe
   class UnsubscribeAck : public Message {
   public:
-    // Construct from a network buffer
+    //! Construct from a network buffer
     UnsubscribeAck(uint8_t* data, uint32_t length);
 
   };
 
 
-  // Ping the broker
+  //! Ping the broker
   class Ping : public Message {
   private:
     uint8_t response_type(void) const { return MQTTPINGRESP; }
 
   public:
-    // Constructor
+    //! Constructor
     Ping() :
       Message(MQTTPINGREQ)
     {}
 
-    // Construct from a network buffer
+    //! Construct from a network buffer
     Ping(uint8_t* data, uint32_t length) :
       Message(MQTTPINGREQ)
     {}
@@ -390,15 +442,15 @@ namespace MQTT {
   };
 
 
-  // Response to Ping
+  //! Response to Ping
   class PingResp : public Message {
   public:
-    // Constructor
+    //! Constructor
     PingResp() :
       Message(MQTTPINGRESP)
     {}
 
-    // Construct from a network buffer
+    //! Construct from a network buffer
     PingResp(uint8_t* data, uint32_t length) :
       Message(MQTTPINGRESP)
     {}
@@ -406,10 +458,10 @@ namespace MQTT {
   };
 
 
-  // Disconnect from the broker
+  //! Disconnect from the broker
   class Disconnect : public Message {
   public:
-    // Constructor
+    //! Constructor
     Disconnect() :
       Message(MQTTDISCONNECT)
     {}
