3d13e7adef69ce87903de5f59ba6640d81105853
thomask <THKDev@users.noreply.github.com>
2017-03-13 19:40:23 +0100

    Fixed PublishAck to send variable header

--- src/MQTT.h
+++ src/MQTT.h
@@ -327,6 +327,9 @@ namespace MQTT {
   //! Response to Publish when qos == 1
   class PublishAck : public Message {
   private:
+    uint32_t variable_header_length(void) const { return sizeof(_packet_id); }
+    void write_variable_header(uint8_t *buf, uint32_t& bufpos) const { write_packet_id(buf, bufpos); }
+
     //! Private constructor from a network buffer
     PublishAck(uint8_t* data, uint32_t length);
 
