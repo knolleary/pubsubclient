baa19a0f6cbd4feb0c546c6493e8cf0d690981c5
Ian Tester <imroykun@gmail.com>
2017-05-16 01:07:09 +1000

    Tests: Pull in dummy defines from the ESP8266 port for AVR's pgmspace types and functions

--- tests/src/lib/Arduino.h
+++ tests/src/lib/Arduino.h
@@ -18,6 +18,29 @@ extern "C"{
 }
 
 #define PROGMEM
-#define pgm_read_byte_near(x) *(x)
+#define PGM_P         const char *
+#define PGM_VOID_P    const void *
+#define FPSTR(p)      ((const char *)(p))
+#define PSTR(s)       (s)
+#define _SFR_BYTE(n)  (n)
+
+#define pgm_read_byte(addr)   (*(const unsigned char *)(addr))
+#define pgm_read_word(addr)   (*(const unsigned short *)(addr))
+#define pgm_read_dword(addr)  (*(const unsigned long *)(addr))
+#define pgm_read_float(addr)  (*(const float *)(addr))
+
+#define pgm_read_byte_near(addr)  pgm_read_byte(addr)
+#define pgm_read_word_near(addr)  pgm_read_word(addr)
+#define pgm_read_dword_near(addr) pgm_read_dword(addr)
+#define pgm_read_float_near(addr) pgm_read_float(addr)
+#define pgm_read_byte_far(addr)   pgm_read_byte(addr)
+#define pgm_read_word_far(addr)   pgm_read_word(addr)
+#define pgm_read_dword_far(addr)  pgm_read_dword(addr)
+#define pgm_read_float_far(addr)  pgm_read_float(addr)
+
+
+class __FlashStringHelper;
+
+inline void yield(void) {}
 
 #endif // Arduino_h
