853057445b310f216a6242df9e9b7e2c55fefb09
Ian Tester <imroykun@gmail.com>
2017-05-16 03:17:06 +1000

    Tests: Pull in more pgmspace.h definitions from the ESP8266 for Arduino.h

--- tests/src/lib/Arduino.h
+++ tests/src/lib/Arduino.h
@@ -9,16 +9,6 @@
 
 typedef std::string String;
 
-extern "C"{
-    typedef uint8_t byte ;
-    typedef uint8_t boolean ;
-
-    /* sketch */
-    extern void setup( void ) ;
-    extern void loop( void ) ;
-    uint32_t millis( void );
-}
-
 #define PROGMEM
 #define PGM_P         const char *
 #define PGM_VOID_P    const void *
@@ -40,6 +30,52 @@ extern "C"{
 #define pgm_read_dword_far(addr)  pgm_read_dword(addr)
 #define pgm_read_float_far(addr)  pgm_read_float(addr)
 
+#define SIZE_IRRELEVANT 0x7fffffff
+
+extern "C"{
+    typedef uint8_t byte ;
+    typedef uint8_t boolean ;
+
+    /* sketch */
+    extern void setup( void ) ;
+    extern void loop( void ) ;
+    uint32_t millis( void );
+
+  inline void* memcpy_P(void* dest, PGM_VOID_P src, size_t count) {
+    const uint8_t* read = reinterpret_cast<const uint8_t*>(src);
+    uint8_t* write = reinterpret_cast<uint8_t*>(dest);
+
+    while (count) {
+      *write++ = pgm_read_byte(read++);
+      count--;
+    }
+
+    return dest;
+  }
+
+  inline char* strncpy_P(char* dest, PGM_P src, size_t size) {
+    const char* read = src;
+    char* write = dest;
+    char ch = '.';
+    while (size > 0 && ch != '\0') {
+      ch = pgm_read_byte(read++);
+      *write++ = ch;
+      size--;
+    }
+
+    return dest;
+  }
+
+  inline size_t strnlen_P(PGM_P s, size_t size) {
+    const char* cp;
+    for (cp = s; size != 0 && pgm_read_byte(cp) != '\0'; cp++, size--);
+    return (size_t) (cp - s);
+  }
+}
+
+#define strcpy_P(dest, src)          strncpy_P((dest), (src), SIZE_IRRELEVANT)
+
+#define strlen_P(strP)          strnlen_P((strP), SIZE_IRRELEVANT)
 
 class __FlashStringHelper;
 
